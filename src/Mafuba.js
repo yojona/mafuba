693d4c6bb7440da1e03cd9193b6c8933ff2284c9
Jonathan Ayala <yojona@msn.com>
2018-07-11 17:43:31 -0700

    Add methods support

--- src/Mafuba.js
+++ src/Mafuba.js
@@ -1,17 +1,31 @@
 export default class Mafuba {
-  constructor (data) {
-    this.data = data
-    this.components = []
+  constructor (object) {
+    this.data = {}
+    this.refs = []
+
+    if (object.hasOwnProperty('data')) this.data = object.data
+    else this.data = object
+
+    if (object.hasOwnProperty('methods')) {
+      for (const method in object.methods) {
+        this.constructor.prototype[method] = object.methods[method]
+      }
+    }
+  }
+
+  getData () {
+    return this.data
   }
 
   setState (data) {
-    this.components.forEach(comp => {
-      comp.setState(data)
-    })
     this.data = Object.assign({}, this.data, data)
+
+    this.refs.forEach(component => {
+      component.setState(data)
+    })
   }
 
   link (component) {
-    this.components.push(component)
+    this.refs.push(component)
   }
 }
